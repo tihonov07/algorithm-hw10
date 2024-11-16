1e33dd773033667e568c921196b4467d685070a5
aleksandr <Aleksandr.A.Tikhonov@rt.ru>
2024-11-16 14:42:04 +0300

    lesson 10

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -5,6 +5,11 @@ import lombok.SneakyThrows;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeSet;
 
 public class HomeWork {
 
@@ -14,9 +19,40 @@ public class HomeWork {
      */
     @SneakyThrows
     public void championship(InputStream in, OutputStream out) {
+            try (var sc = new Scanner(in); var pw = new PrintWriter(out)) {
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+
+                int[] resultArr = new int[n + 1];
+                TreeSet<Integer> knights = new TreeSet<>();
+                for (int i = 1; i <= n; i++) {
+                    knights.add(i);
+                }
+
+                for (int i = 0; i < m; i++) {
+                    int l = sc.nextInt();
+                    int r = sc.nextInt();
+                    int x = sc.nextInt();
+
+                    var participants = knights.subSet(l, true, r, true);
+
+                    List<Integer> toRemove = new ArrayList<>();
+                    for (int knight : participants) {
+                        if (knight != x) {
+                            resultArr[knight] = x;
+                            toRemove.add(knight);
+                        }
+                    }
+
+                    toRemove.forEach(knights::remove);
+            }
+
+            for (int i = 1; i <= n; i++) {
+                pw.print(resultArr[i]);
+                if (i < n) pw.print(" ");
+            }
+        }
 
     }
-
-
 
 }
